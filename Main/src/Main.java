a5bfa4c33264cba3f59b15c3a82e86596a8d2185
mTwTm <osimpleo@gmail.com>
2015-03-23 15:31:40 +0800

    An old solution

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,18 +1,63 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 2015-03-23
+// UVa 105 The Skyline Problem
+// Current Status: Accepted
+// Last Submitted: 2015-03-23 07:07:46
 // Run Time: 0.775
 import java.util.Scanner;
 
-public class Main {
+
+class Main {
 	
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		while(s.hasNextLine())
+		Scanner s;				
+		s = new Scanner(System.in);
+		int[] height = new int[10000];
+		int rightMost = 0;
+		int leftMost = 10000;
+		while(s.hasNextInt())
 		{
-			System.out.println();
+			int l = s.nextInt();
+			int h = s.nextInt();
+			int r = s.nextInt();
+			if(r-1>rightMost)
+				rightMost = r-1;
+			if(l<leftMost)
+				leftMost = l;
+			for(int i=l;i<r;i++)
+			{
+				if(height[i]<h)
+					height[i] = h;
+			}
 		}
 		s.close();
+		/*for(int i=leftMost;i<=rightMost;i++)
+		{
+			System.out.printf("%3d",i);
+		}
+		System.out.println();
+		for(int i=leftMost;i<=rightMost;i++)
+		{
+			System.out.printf("%3d",height[i]);
+		}
+		System.out.println();*/
+		
+		int currHeight = -1;
+		for(int i=leftMost;i<=rightMost;i++)
+		{
+			if(height[i]!=currHeight)
+			{
+				System.out.print(i);
+				System.out.print(" ");
+				System.out.print(height[i]);
+				System.out.print(" ");
+				currHeight = height[i];
+			}
+		}
+		if(height[rightMost]!=0)
+		{
+			System.out.print(rightMost+1);
+			System.out.print(" ");
+			System.out.print(0);
+		}
+		System.out.println();
 	}
-	
 }
