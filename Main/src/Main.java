6bd6c35315399954574f5dde293096627ff9862f
mTwTm <osimpleo@gmail.com>
2015-03-23 16:06:21 +0800

    First submit

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -6,11 +6,34 @@ import java.util.Scanner;
 
 public class Main {
 	
+	private static int calLength(int target)
+	{
+		int count = 1;
+		while(target!=1)
+		{
+			count++;
+			if(target%2==1)
+				target = 3 * target +1;
+			else
+				target = target / 2;
+		}
+		return count;
+	}
+	
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		while(s.hasNextLine())
+		while(s.hasNextInt())
 		{
-			System.out.println();
+			int i = s.nextInt();
+			int j = s.nextInt();
+			int maxCycle = 0;
+			for(int c = i ; c<=j ; c++)
+			{
+				int result = calLength(c);
+				if(result>maxCycle)
+					maxCycle = result;
+			}
+			System.out.println(i+" "+j+" "+maxCycle);
 		}
 		s.close();
 	}
