b6f64b9f220079107d5098132c9d1c5518c0c6f0
mTwTm <osimpleo@gmail.com>
2015-03-23 17:51:42 +0800

    Ready to code

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,4 +1,4 @@
-// UVa <ID> <Name> 
+// UVa 101 The Blocks Problem
 // Current Status: Accepted|Pending|TooSlow
 // Last Submitted: 2015-03-23
 // Run Time: 0.775
@@ -8,8 +8,10 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		int blocks = Integer.parseInt(s.nextLine());
 		while(s.hasNextLine())
-		{
+		{			
+			String nextCmd = s.nextLine();
 			System.out.println();
 		}
 		s.close();
