5fc0b1dca63722e60ef2b61557c95ed9592717e4
mtwtm <mtwtm@msn.com>
2015-03-25 16:04:33 +0800

    Add submit result: Wrong Answer

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
 // UVa 101 The Blocks Problem
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 2015-03-23
-// Run Time: 0.775
+// Current Status: Wrong Answer
+// Last Submitted: 2015-03-25 08:03:18
+// Run Time: 0.955
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
