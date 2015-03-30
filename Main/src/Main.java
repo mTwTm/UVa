325efb7f3ed4df269c2668531354a4f072cbccfe
mtwtm <mtwtm@msn.com>
2015-03-31 00:10:08 +0800

    Old file

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,24 +1,42 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 138 Street Numbers 
+// Current Status: Accepted
+// Last Submitted: 2015-03-30 16:00:27
+// Run Time: 0.695
 
-import java.util.Scanner;
-
-public class Main {
+class Main {
 	
-	private void run(){
-		Scanner s = new Scanner(System.in);
-		while(s.hasNextLine())
+	public static long safeSqrt(long square){
+		long rough = (long) Math.sqrt(square);
+		for(int i=-1;i<=1;i++)
 		{
-			System.out.println();
+			if( (rough+i) * (rough+i) == square)
+				return rough+i;
 		}
-		s.close();
+		return -1;
 	}
 	
+	public static String smartPrint(long i){
+		String s = Long.toString(i);
+		while(s.length()<10)
+		{
+			s = " " + s;
+		}
+		return s;
+	}
+
 	public static void main(String[] args) {
-		Main myself = new Main();
-		myself.run();
+		int counter = 0;
+		for(long i=2;counter<10;i++)
+		{
+			long sqrt = safeSqrt( (2*i+1)*(2*i+1)-4*(i-i*i) ); 
+			if( sqrt!=-1)
+			{
+				long k = ( sqrt - (2*i+1) ) / 2; 
+				if(k<=0)
+					continue;
+				System.out.println(smartPrint(i)+smartPrint(i+k));
+				counter++;
+			}
+		}
 	}
-	
 }
