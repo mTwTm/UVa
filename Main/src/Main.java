79054f9cd286fbc44fe4da8d5c7d2f8a1d617462
mtwtm <mtwtm@msn.com>
2015-03-31 00:27:00 +0800

    Old file

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,24 +1,57 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 151 Power Crisis 
+// Current Status: Accepted
+// Last Submitted: 2015-03-30 16:25:00
+// Run Time: 0.212
+
 
 import java.util.Scanner;
 
-public class Main {
+class Main {
 	
-	private void run(){
-		Scanner s = new Scanner(System.in);
-		while(s.hasNextLine())
+	public static void main(String[] args) {
+		Scanner s;		
+		s = new Scanner(System.in);				
+		while(s.hasNextInt())
 		{
-			System.out.println();
+			int n = s.nextInt();
+			if(n==0)
+				break;
+			boolean[] shut = new boolean[n];
+			int currentShut = 0;
+			int testNum = 1;
+			cal:
+			while(true)
+			{
+				if(currentShut==12)
+				{
+					shut[currentShut]=true;
+					for(int i=0;i<n;i++)
+					{
+						if(shut[i]==false)
+						{
+							testNum++;
+							currentShut = 0;
+							for(int j=0;j<n;j++)
+								shut[j] = false;
+							continue cal;
+						}
+					}
+					System.out.println(testNum);
+					break;
+				}
+				else
+				{
+					shut[currentShut]=true;
+					for(int i=0;i<testNum;i++)
+					{
+						currentShut = (currentShut+1)%n;
+						if(shut[currentShut]==true)
+							i--;
+					}
+				}
+			}
 		}
 		s.close();
 	}
-	
-	public static void main(String[] args) {
-		Main myself = new Main();
-		myself.run();
-	}
-	
+
 }
