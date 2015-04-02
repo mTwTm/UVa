6707a409782aa6ec63debf9f8ea57ff580eaa5b5
mtwtm <mtwtm@msn.com>
2015-04-02 16:33:06 +0800

    AC!

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,17 +1,45 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 256 Quirksome Squares
+// Current Status: Accepted
+// Last Submitted: 2015-04-02 08:29:59
+// Run Time: 0.715
 
 import java.util.Scanner;
 
 public class Main {
 	
+	StringBuilder answ[];
+	
 	private void run(){
-		Scanner s = new Scanner(System.in);
-		while(s.hasNextLine())
+		answ = new StringBuilder[4];
+		for(int i=0;i<4;i++)
 		{
-			System.out.println();
+			answ[i] = new StringBuilder();
+			int num = (i+1)*2;
+			int upperbound = 1;
+			for(int j=0;j<num/2;j++)
+				upperbound *= 10;
+			for(int j=0;j<upperbound;j++)
+			{
+				for(int k=0;k<upperbound;k++)
+				{
+					if((j+k)*(j+k)==j*upperbound+k)
+					{
+						if(num==2)
+							answ[i].append(String.format("%02d", j*upperbound+k)+"\n");
+						if(num==4)
+							answ[i].append(String.format("%04d", j*upperbound+k)+"\n");
+						if(num==6)
+							answ[i].append(String.format("%06d", j*upperbound+k)+"\n");
+						if(num==8)
+							answ[i].append(String.format("%08d", j*upperbound+k)+"\n");
+					}						
+				}
+			}
+		}
+		Scanner s = new Scanner(System.in);		
+		while(s.hasNextInt())
+		{			
+			System.out.print(answ[s.nextInt()/2-1]);
 		}
 		s.close();
 	}
