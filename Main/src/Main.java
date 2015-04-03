23c3d513af735f8aec449433a628391f50345ed4
mtwtm <mtwtm@msn.com>
2015-04-03 17:06:07 +0800

    New template for faster I/O

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,17 +3,34 @@
 // Last Submitted: 
 // Run Time: 
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main {
 	
 	private void run(){
-		Scanner s = new Scanner(System.in);
-		while(s.hasNextLine())
-		{
-			System.out.println();
+		
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner s = new Scanner(r);
+		try{
+			while( s.hasNextLine() )
+			{
+				String line = s.nextLine();
+				w.write(line);
+			}
+			r.close();
+			w.close();
 		}
-		s.close();
+		catch(IOException ex)
+		{
+			//Nothing to do
+		}
+		
 	}
 	
 	public static void main(String[] args) {
