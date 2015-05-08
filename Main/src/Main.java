e837d9dcc720aa46b35fc8f7a8257dc1def8c7c1
mtwtm <mtwtm@msn.com>
2015-05-08 13:57:49 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,13 +1,14 @@
 // UVa 369 Combinations
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// Current Status: Accepted
+// Last Submitted: 2015-05-08 05:56:10
+// Run Time: 0.452
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 
 public class Main {
 	
@@ -23,7 +24,18 @@ public class Main {
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null )
 			{
-				w.write(nextLine);
+                String[] args = nextLine.trim().split("\\s+");
+                int N = Integer.parseInt(args[0]);
+                int M = Integer.parseInt(args[1]);
+                if(N==0&&M==0)
+                    break;
+                int smallerM = N-M<M ? N-M : M;
+                BigInteger count = BigInteger.ONE;
+                for(int i=0;i<smallerM;i++)
+                    count = count.multiply(BigInteger.valueOf(N-i));
+                for(int i=0;i<smallerM;i++)
+                    count = count.divide(BigInteger.valueOf(i + 1));
+				w.write( N +" things taken " + M + " at a time is " + count.toString() + " exactly.\n");
 			}
 			r.close();
 			w.close();
