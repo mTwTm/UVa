13e6001e8b5bb16125e7ba10febb2dda54e0f89c
mtwtm <mtwtm@msn.com>
2015-05-26 19:03:44 +0800

    Add charset to be accepted

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,29 +1,33 @@
 // UVa 458 The Decoder
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// Current Status: Accepted
+// Last Submitted: 2015-05-26 11:01:14
+// Run Time: 0.376
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
 	
-	private BufferedReader r; 
+	private BufferedReader r;
 	private BufferedWriter w;
+	private final int hi = 0;
 	
 	private void run(){
-		
-		r = new BufferedReader(new InputStreamReader(System.in)); 
-		w = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		try {
+			r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
+			w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	
 		try{
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null )
 			{
-				w.write(nextLine);
+				StringBuilder sb = new StringBuilder(nextLine);
+				for(int i=0;i<sb.length();i++)
+					sb.setCharAt(i, (char) (sb.charAt(i) + ('i'-'p') ));
+				w.write(sb.toString()+"\n");
 			}
 			r.close();
 			w.close();
