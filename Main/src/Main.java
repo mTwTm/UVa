7f05deae215467c9949c1a08fe39e8e3d8ba8448
mtwtm <mtwtm@msn.com>
2015-05-28 10:34:31 +0800

    WA (problem for java answer?)

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -8,22 +8,47 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 	
 	private BufferedReader r; 
 	private BufferedWriter w;
-	
+
+	private boolean isVowel(char c) {
+		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+			return true;
+		else if(c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+			return true;
+		return false;
+	}
+
 	private void run(){
-		
-		r = new BufferedReader(new InputStreamReader(System.in)); 
-		w = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
 	
 		try{
+			r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
+			w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null )
 			{
-				w.write(nextLine);
+				int begin = 0; 
+				Matcher matcher = Pattern.compile("[a-zA-Z]+").matcher(nextLine);
+				while(matcher.find())
+				{
+					StringBuilder word = new StringBuilder(matcher.group());
+					if(!isVowel(word.charAt(0))) {
+						word.append(word.charAt(0));
+						word.replace(0, 1, "");
+					}
+					word.append("ay");
+					w.write(nextLine.substring(begin, matcher.start()));
+					begin = matcher.end();
+					w.write(word.toString());
+				}
+				w.write(nextLine.substring(begin, nextLine.length())+"\n");
 			}
 			r.close();
 			w.close();
