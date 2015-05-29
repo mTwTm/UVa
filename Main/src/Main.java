c7db5b5e06a5f43d91e9632d0dbe4665c41ccde3
mtwtm <mtwtm@msn.com>
2015-05-29 14:28:24 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 568 Just the Facts
+// Current Status: Accepted
+// Last Submitted: 2015-05-29 06:18:59
+// Run Time: 0.192
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -13,6 +13,7 @@ public class Main {
 	
 	private BufferedReader r; 
 	private BufferedWriter w;
+	private int[] answer = new int[10001];
 	
 	private void run(){
 
@@ -21,9 +22,20 @@ public class Main {
 			r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
+			answer[0] = 1;
+			for(int i=1;i<10001;i++) {
+				int clone = i;
+				while(clone%10==0)
+					clone /= 10;
+				answer[i] = ( answer[i-1] * clone );
+				while(answer[i]%10==0)
+					answer[i] /= 10;
+				answer[i] = answer[i] % 100000;
+			}
 			while( ( nextLine = r.readLine() ) != null )
 			{
-				w.write(nextLine);
+				int next = Integer.parseInt(nextLine);
+				w.write(String.format("%5d", next) + " -> " + answer[next]%10 + "\n");
 			}
 			r.close();
 			w.close();
