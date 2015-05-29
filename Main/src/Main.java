f96f6e7487caaf94de977117260ad8bd3f854828
mtwtm <mtwtm@msn.com>
2015-05-29 13:25:31 +0800

    Old AC code

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,43 +1,67 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 541 Error Correction
+// Current Status: Accepted
+// Last Submitted: 2015-05-29 05:24:19
+// Run Time: 0.826
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
-public class Main {
-	
-	private BufferedReader r; 
-	private BufferedWriter w;
-	
-	private void run(){
 
-		try{
+class Main {
 
-			r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
-			w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
-			String nextLine;
-			while( ( nextLine = r.readLine() ) != null )
-			{
-				w.write(nextLine);
-			}
-			r.close();
-			w.close();
-		}
-		catch(IOException ex)
-		{
-			//Nothing to do
-		}
-		
-	}
-	
 	public static void main(String[] args) {
-		Main myself = new Main();
-		myself.run();
+		Scanner s;
+		s = new Scanner(System.in);
+		while(s.hasNextInt())
+		{
+			int size = s.nextInt();
+			if(size==0)
+				break;
+			int[] row = new int[size];
+			int[] col = new int[size];
+			for(int i=0;i<size;i++)
+			{
+				for(int j=0;j<size;j++)
+				{
+					int next = s.nextInt();
+					if(next==1)
+					{
+						row[j]++;
+						col[i]++;
+					}
+				}
+			}
+			boolean changable = true;
+			int corruptRow = -1;
+			int corruptCol = -1;
+			for(int i=0;i<size;i++)
+			{
+				if(row[i]%2==1)
+				{
+					if(corruptRow!=-1)
+					{
+						changable = false;
+						break;
+					}
+					corruptRow = i;
+				}
+				if(col[i]%2==1)
+				{
+					if(corruptCol!=-1)
+					{
+						changable = false;
+						break;
+					}
+					corruptCol = i;
+				}
+			}
+			if(corruptRow==-1&&corruptCol==-1)
+				System.out.println("OK");
+			else if(changable)
+				System.out.println("Change bit ("+(corruptCol+1)+","+(corruptRow+1)+")");
+			else
+				System.out.println("Corrupt");
+
+		}
+		s.close();
 	}
-	
 }
