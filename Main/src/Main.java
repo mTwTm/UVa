924b39a4588e86d65a749709c64d1927be2d36cc
mtwtm <mtwtm@msn.com>
2015-05-29 13:18:17 +0800

    Directly cloned from (UVa 369 Combinations)

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,29 +1,41 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 530 Binomial Showdown
+// Current Status: Accepted
+// Last Submitted: 2015-05-29 05:17:43
+// Run Time: 0.178
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 
 public class Main {
-	
-	private BufferedReader r; 
+
+	private BufferedReader r;
 	private BufferedWriter w;
-	
+
 	private void run(){
 
-		try{
+		r = new BufferedReader(new InputStreamReader(System.in));
+		w = new BufferedWriter(new OutputStreamWriter(System.out));
 
-			r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
-			w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
+		try{
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null )
 			{
-				w.write(nextLine);
+				String[] args = nextLine.trim().split("\\s+");
+				int N = Integer.parseInt(args[0]);
+				int M = Integer.parseInt(args[1]);
+				if(N==0&&M==0)
+					break;
+				int smallerM = N-M<M ? N-M : M;
+				BigInteger count = BigInteger.ONE;
+				for(int i=0;i<smallerM;i++)
+					count = count.multiply(BigInteger.valueOf(N-i));
+				for(int i=0;i<smallerM;i++)
+					count = count.divide(BigInteger.valueOf(i + 1));
+				w.write(count.toString() + "\n");
 			}
 			r.close();
 			w.close();
@@ -32,12 +44,12 @@ public class Main {
 		{
 			//Nothing to do
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		Main myself = new Main();
 		myself.run();
 	}
-	
+
 }
