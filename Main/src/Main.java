3fb63c282d8f87da70268ce0749c410174fdaa90
mtwtm <mtwtm@msn.com>
2015-06-01 20:55:26 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,18 +1,20 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 543 Goldbach's Conjecture
+// Current Status: Accepted
+// Last Submitted: 2015-06-01 12:52:56
+// Run Time: 0.853
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Main {
 	
 	private BufferedReader r; 
 	private BufferedWriter w;
+	private Map<Integer, Boolean> primes;
 	
 	private void run(){
 
@@ -21,9 +23,32 @@ public class Main {
 			r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
+			primes = new LinkedHashMap<>();
+			primes.put(2, true);
+primeAdd:
+			for(int i=3;i<=1_000_000;i+=2) {
+				for(int nextPrime : primes.keySet()) {
+					if(nextPrime>Math.sqrt(i)) {
+						primes.put(i, true);
+						break;
+					}
+					if(i%nextPrime==0)
+						continue primeAdd;
+				}
+			}
+
 			while( ( nextLine = r.readLine() ) != null )
 			{
-				w.write(nextLine);
+				int next = Integer.parseInt(nextLine);
+				if(next==0)
+					break;
+				for(int i : primes.keySet()) {
+					if(primes.containsKey(next - i))
+					{
+						w.write(next + " = " + i + " + " + (next-i) + "\n");
+						break;
+					}
+				}
 			}
 			r.close();
 			w.close();
