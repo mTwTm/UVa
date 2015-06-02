d0f87ed545cbb3e507cdfdb509d223a0c33f1d69
mtwtm <mtwtm@msn.com>
2015-06-02 11:13:51 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 573 The Snail
+// Current Status: Accepted
+// Last Submitted: 2015-06-02 03:08:42
+// Run Time: 0.115
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -13,6 +13,13 @@ public class Main {
 	
 	private BufferedReader r; 
 	private BufferedWriter w;
+	private int current;
+	private int  efficiency;
+	private int  up;
+	private int  down;
+	private int  target;
+	private int  day;
+	private int fatigue;
 	
 	private void run(){
 
@@ -23,7 +30,28 @@ public class Main {
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null )
 			{
-				w.write(nextLine);
+				String[] args = nextLine.split("\\s+");
+				day = 1;
+				current = 0;
+				target = Integer.parseInt(args[0])*100;
+				up = Integer.parseInt(args[1])*100;
+				down = Integer.parseInt(args[2])*100;
+				fatigue = Integer.parseInt(args[3]);
+				efficiency = 100;
+				if(target==0)
+					break;
+				boolean success = false;
+				boolean failure = false;
+				while (true) {
+					success = (current+up*efficiency/100) > target;
+					failure = (current+up*efficiency/100-down) < 0;
+					if(success||failure)
+						break;
+					current += up*efficiency/100-down;
+					efficiency = (efficiency-fatigue>=0) ? efficiency-fatigue : 0;
+					day++;
+				}
+				w.write( (success ? "success" : "failure" ) + " on day " + day + "\n");
 			}
 			r.close();
 			w.close();
