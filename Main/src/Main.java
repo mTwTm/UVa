11ba156b04e85cae447cbefbf5989522fbed1abc
mtwtm <mtwtm@msn.com>
2015-06-02 18:56:50 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,18 +1,22 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 623 500!
+// Current Status: Accepted
+// Last Submitted: 2015-06-02 10:54:47
+// Run Time: 1.086
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 	
 	private BufferedReader r; 
 	private BufferedWriter w;
+	private List<BigInteger> answers;
 	
 	private void run(){
 
@@ -21,9 +25,15 @@ public class Main {
 			r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
+			answers = new ArrayList<>();
+			answers.add(BigInteger.ONE);
+			for(int count = 1;count<=1000; count++)
+				answers.add(answers.get(answers.size() - 1).multiply(BigInteger.valueOf(count)));
+
 			while( ( nextLine = r.readLine() ) != null )
 			{
-				w.write(nextLine);
+				w.write(nextLine.trim()+"!\n");
+				w.write(answers.get(Integer.parseInt(nextLine.trim())).toString()+"\n");
 			}
 			r.close();
 			w.close();
