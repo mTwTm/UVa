dca22e17a194e411ccf06cbad6af216017c5c752
mtwtm <mtwtm@msn.com>
2015-06-02 20:04:28 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,19 +1,21 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 673 Parentheses Balance
+// Current Status: Accepted
+// Last Submitted: 2015-06-02 12:02:56
+// Run Time: 0.613
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class Main {
-	
-	private BufferedReader r; 
+
+	private BufferedReader r;
 	private BufferedWriter w;
-	
+	private Stack<Character> input;
+
 	private void run(){
 
 		try{
@@ -23,7 +25,54 @@ public class Main {
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null )
 			{
-				w.write(nextLine);
+				int count = Integer.parseInt(nextLine);
+				for(int i=0 ; i<count ; i++) {
+					nextLine = r.readLine();
+					boolean isBalance = true;
+					input = new Stack<>();
+					for(int j=0 ; j<nextLine.length() ; j++) {
+
+						char head = input.size()==0 ? '\0' : input.peek();
+						char next = nextLine.charAt(j);
+						if(head!='\0') {
+							if(head=='(') {
+								if(next=='('||next=='[')
+									input.push(next);
+								else if(next==')') {
+									input.pop();
+									continue;
+								}
+								else if(next==']') {
+									isBalance = false;
+									break;
+								}
+							}
+							else if (head == '[') {
+								if(next=='('||next=='[')
+									input.push(next);
+								else if(next==']') {
+									input.pop();
+									continue;
+								}
+								else if(next==')') {
+									isBalance = false;
+									break;
+								}
+							}
+						}
+						else {
+							input.push(next);
+						}
+					}
+					if(input.size()!=0)
+						isBalance = false;
+					if(isBalance) {
+						w.write("Yes\n");
+					}
+					else {
+						w.write("No\n");
+					}
+				}
 			}
 			r.close();
 			w.close();
@@ -32,12 +81,12 @@ public class Main {
 		{
 			//Nothing to do
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		Main myself = new Main();
 		myself.run();
 	}
-	
+
 }
