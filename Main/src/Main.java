69c75f4ef4c7101021eaf2944a4be7341d865907
mtwtm <mtwtm@msn.com>
2015-06-03 19:56:22 +0800

    Old AC Code (showing PE but might be UVa Judge's problem)

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,43 +1,117 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
+// UVa 706 LCD Display
+// Current Status: Accepted
 // Last Submitted: 
 // Run Time: 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+//UVA 706 - LCD Display
+import java.util.Scanner;
 
-public class Main {
-	
-	private BufferedReader r; 
-	private BufferedWriter w;
-	
-	private void run(){
 
-		try{
+class Main {
 
-			r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
-			w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
-			String nextLine;
-			while( ( nextLine = r.readLine() ) != null )
-			{
-				w.write(nextLine);
-			}
-			r.close();
-			w.close();
-		}
-		catch(IOException ex)
-		{
-			//Nothing to do
-		}
-		
-	}
-	
 	public static void main(String[] args) {
-		Main myself = new Main();
-		myself.run();
+		final boolean[][] digitSegments= {
+				{true,  true,  true, true,  true,  true,  false},
+				{false, true,  true, false, false, false, false},
+				{true,  true, false, true,  true,  false, true },
+				{true,  true,  true, true,  false, false, true },
+				{false, true,  true, false, false, true,  true },
+				{true,  false, true, true,  false, true,  true },
+				{true,  false, true, true,  true,  true,  true },
+				{true,  true , true, false, false, false, false},
+				{true,  true , true, true,  true,  true,  true },
+				{true,  true , true, true,  false, true,  true },
+		};
+		Scanner s = new Scanner(System.in);
+		boolean first = true;
+		while(s.hasNextInt())
+		{
+			int size = s.nextInt();
+			String [] printBuffer = new String[2*size+3];
+			for(int i=0;i<printBuffer.length;i++)
+				printBuffer[i] = "";
+			int number = s.nextInt();
+			if(number==0&&size==0)
+				break;
+			int[] numberSplit = new int[8];
+			for(int i=0;i<8;i++)
+			{
+				numberSplit[i]=-1;
+			}
+			if(number==0)
+				numberSplit[7]=0;
+			for(int i=7;number!=0;i--)
+			{
+				numberSplit[i] = number%10;
+				number/=10;
+			}
+			for(int i=0;i<8;i++){
+				if(numberSplit[i]==-1)
+					continue;
+				printBuffer[0] = printBuffer[0].concat(" ");
+				for(int j=0;j<size;j++)
+				{
+					if(digitSegments[numberSplit[i]][0]==true)
+						printBuffer[0] = printBuffer[0].concat("-");
+					else
+						printBuffer[0] = printBuffer[0].concat(" ");
+				}
+				printBuffer[0] = printBuffer[0].concat(" "); //Print part a;
+				for(int j=0;j<size;j++)
+				{
+					if(digitSegments[numberSplit[i]][5]==true)
+						printBuffer[1+j] = printBuffer[1+j].concat("|");
+					else
+						printBuffer[1+j] = printBuffer[1+j].concat(" ");
+					for(int k=0;k<size;k++)
+						printBuffer[1+j] = printBuffer[1+j].concat(" ");
+					if(digitSegments[numberSplit[i]][1]==true)
+						printBuffer[1+j] = printBuffer[1+j].concat("|");
+					else
+						printBuffer[1+j] = printBuffer[1+j].concat(" ");
+				}//Print part b&f;
+				printBuffer[size+1] = printBuffer[size+1].concat(" ");
+				for(int j=0;j<size;j++)
+				{
+					if(digitSegments[numberSplit[i]][6]==true)
+						printBuffer[size+1] = printBuffer[size+1].concat("-");
+					else
+						printBuffer[size+1] = printBuffer[size+1].concat(" ");
+				}
+				printBuffer[size+1] = printBuffer[size+1].concat(" ");//Print part g;
+				for(int j=0;j<size;j++)
+				{
+					if(digitSegments[numberSplit[i]][4]==true)
+						printBuffer[size+2+j] = printBuffer[size+2+j].concat("|");
+					else
+						printBuffer[size+2+j] = printBuffer[size+2+j].concat(" ");
+					for(int k=0;k<size;k++)
+						printBuffer[size+2+j] = printBuffer[size+2+j].concat(" ");
+					if(digitSegments[numberSplit[i]][2]==true)
+						printBuffer[size+2+j] = printBuffer[size+2+j].concat("|");
+					else
+						printBuffer[size+2+j] = printBuffer[size+2+j].concat(" ");
+				}//Print part e&c;
+				printBuffer[2*size+2] = printBuffer[2*size+2].concat(" ");
+				for(int j=0;j<size;j++)
+				{
+					if(digitSegments[numberSplit[i]][3]==true)
+						printBuffer[2*size+2] = printBuffer[2*size+2].concat("-");
+					else
+						printBuffer[2*size+2] = printBuffer[2*size+2].concat(" ");
+				}
+				printBuffer[2*size+2] = printBuffer[2*size+2].concat(" "); //Print part g;
+				if(i!=7){
+					for(int j=0;j<printBuffer.length;j++)
+					{
+						printBuffer[j] = printBuffer[j].concat(" ");
+					}
+				}
+			}
+			for(int i=0;i<printBuffer.length;i++)
+				System.out.println(printBuffer[i]);
+			System.out.println();
+		}
+		s.close();
 	}
-	
 }
