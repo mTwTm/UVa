8d329af4c63e1eb23b8d1434634cbcfc7e894308
mTwTm <osimpleo@gmail.com>
2016-02-23 20:36:06 +0800

    Update coding style

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -10,34 +10,25 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
-	
-	private BufferedReader r; 
-	private BufferedWriter w;
-	
-	private void run(){
 
-		try{
+	private void run() {
+		try {
 
-			r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
-			w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
+			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
+			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
-			while( ( nextLine = r.readLine() ) != null )
-			{
+			while( ( nextLine = r.readLine() ) != null ) {
 				w.write(nextLine);
 			}
 			r.close();
 			w.close();
-		}
-		catch(IOException ex)
-		{
+		} catch(IOException ex) {
 			//Nothing to do
 		}
-		
 	}
 	
 	public static void main(String[] args) {
 		Main myself = new Main();
 		myself.run();
 	}
-	
 }
