5b5663eb35976fa8901064d8e03163e42498bba6
mTwTm <osimpleo@gmail.com>
2016-03-27 17:17:52 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 11586 Train Tracks
+// Current Status: Accepted
+// Last Submitted: 2016-03-27 09:17:04
+// Run Time: 0.042
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -11,14 +11,34 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 
+	private int[] count = new int[4];
+	private static final String[] types = {"FF", "FM", "MF", "MM"};
+
 	private void run() {
 		try {
 
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
-			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+			nextLine = r.readLine();
+			int caseCount = Integer.parseInt(nextLine);
+			for(int i=0 ; i<caseCount ; i++) {
+				count = new int[4];
+				nextLine = r.readLine();
+				for(String s : nextLine.split("\\s+")) {
+					for(int j=0 ; j<types.length ; j++) {
+						if(types[j].equals(s)) {
+							count[j]++;
+						}
+					}
+				}
+				int sum = count[0] + count[1] + count[2] + count[3];
+				boolean isFFMMpaired = count[0] == count[3];
+				if(sum==1 || !isFFMMpaired) {
+					w.write("NO LOOP\n");
+				} else {
+					w.write("LOOP\n");
+				}
 			}
 			r.close();
 			w.close();
