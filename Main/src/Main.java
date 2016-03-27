86a81bbc1703670d4c9ff31c24b6a49ac049f0e2
mTwTm <osimpleo@gmail.com>
2016-03-27 15:13:18 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,13 +1,14 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 11727 Cost Cutting
+// Current Status: Accepted
+// Last Submitted: 2016-03-27 07:10:42
+// Run Time: 0.042
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
 
@@ -17,8 +18,17 @@ public class Main {
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
-			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+			nextLine = r.readLine();
+			int count = Integer.parseInt(nextLine);
+			for(int i=0 ; i<count ; i++) {
+				nextLine = r.readLine();
+				String[] intStrings = nextLine.split("\\D+");
+				int[] intValues = new int[3];
+				for(int j = 0 ; j < 3 ; j++) {
+					intValues[j] = Integer.parseInt(intStrings[j]);
+				}
+				Arrays.sort(intValues);
+				w.write("Case " + (i+1) + ": "+intValues[1]+"\n");
 			}
 			r.close();
 			w.close();
