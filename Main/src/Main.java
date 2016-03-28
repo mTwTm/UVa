9c581a7096fdf8004ec18a3e81ec4d601b8e9d05
mTwTm <osimpleo@gmail.com>
2016-03-28 22:03:32 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 11547 Automatic Answer
+// Current Status: Accepted
+// Last Submitted: 2016-03-28 14:03:06
+// Run Time: 0.042
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -16,9 +16,11 @@ public class Main {
 
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
-			String nextLine;
-			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+			int caseCount = Integer.parseInt(r.readLine());
+			for(int c=0 ; c<caseCount ; c++) {
+				int num = Integer.parseInt(r.readLine());
+				int ans = ( num * 567 / 9 + 7492 ) * 235 / 47 - 498;
+				w.write(Math.abs(ans/10)%10+"\n");
 			}
 			r.close();
 			w.close();
