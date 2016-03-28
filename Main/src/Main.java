5fe40094da89d948f4de63df8d25bbfa7d1446dc
mTwTm <osimpleo@gmail.com>
2016-03-28 22:21:57 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,13 +1,9 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 11530 SMS Typing
+// Current Status: Accepted
+// Last Submitted: 2016-03-28 14:19:38
+// Run Time: 0.038
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
 
@@ -16,9 +12,50 @@ public class Main {
 
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
-			String nextLine;
-			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+			int caseCount = Integer.parseInt(r.readLine());
+			for(int c=0 ; c<caseCount ; c++) {
+				String nextLine = r.readLine();
+				int sum = 0;
+				for(int i=0 ; i<nextLine.length() ; i++) {
+					switch (nextLine.charAt(i)) {
+						case 'a':
+						case 'd':
+						case 'g':
+						case 'j':
+						case 'm':
+						case 'p':
+						case 't':
+						case 'w':
+						case ' ':
+							sum+=1;
+							break;
+						case 'b':
+						case 'e':
+						case 'h':
+						case 'k':
+						case 'n':
+						case 'q':
+						case 'u':
+						case 'x':
+							sum+=2;
+							break;
+						case 'c':
+						case 'f':
+						case 'i':
+						case 'l':
+						case 'o':
+						case 'r':
+						case 'v':
+						case 'y':
+							sum+=3;
+							break;
+						case 's':
+						case 'z':
+							sum+=4;
+							break;
+					}
+				}
+				w.write("Case #" + (c+1) + ": " + sum + "\n");
 			}
 			r.close();
 			w.close();
