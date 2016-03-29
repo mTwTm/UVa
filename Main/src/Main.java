6114a1397799e39f12512eb3f1492ebae65c6b00
mTwTm <osimpleo@gmail.com>
2016-03-30 00:00:07 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 11462 Age Sort
+// Current Status: Accepted
+// Last Submitted: 2016-03-29 13:04:01
+// Run Time: 0.719
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -11,14 +11,34 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 
+	private static int MAX_AGE = 99;
+
 	private void run() {
 		try {
 
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
-			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+			while(true) {
+				nextLine = r.readLine();
+				int totalPeople = Integer.parseInt(nextLine);
+				if(totalPeople == 0) {
+					break;
+				}
+				int[] ageCount = new int[MAX_AGE+1];
+				nextLine = r.readLine();
+				String[] ages = nextLine.split("\\s+");
+				for(int i=0 ; i<totalPeople ; i++) {
+					ageCount[Integer.parseInt(ages[i])]++;
+				}
+				StringBuilder answer = new StringBuilder();
+				for(int i=1 ; i<=MAX_AGE ; i++) {
+					for(int j=0 ; j<ageCount[i] ; j++) {
+						answer.append(i).append(" ");
+					}
+				}
+				answer.replace(answer.length()-1, answer.length(), "");
+				w.write(answer.toString()+"\n");
 			}
 			r.close();
 			w.close();
