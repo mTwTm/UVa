e8f2f9f7f62017b8e560a2cabc23b7bb6a1a6bcb
mTwTm <osimpleo@gmail.com>
2016-04-06 21:50:30 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 11332 Summing Digits
+// Current Status: Accepted
+// Last Submitted: 2016-04-06 13:48:58
+// Run Time: 0.030
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -16,9 +16,22 @@ public class Main {
 
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
-			String nextLine;
-			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+			while(true) {
+				String nextLine = r.readLine();
+				if(nextLine.equals("0")) {
+					break;
+				} else {
+					int num = Integer.parseInt(nextLine);
+					while(num>=10) {
+						int digitSum = 0;
+						while( num> 0) {
+							digitSum += num % 10;
+							num /= 10;
+						}
+						num = digitSum;
+					}
+					w.write("" + num + "\n");
+				}
 			}
 			r.close();
 			w.close();
