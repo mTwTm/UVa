d6a11b94cd16eb285b92edb9a0f63cc888615b17
mTwTm <osimpleo@gmail.com>
2016-04-13 04:47:51 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10812 Beat the Spread!
+// Current Status: Accepted
+// Last Submitted: 2016-04-12 20:48:51
+// Run Time: 0.040
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -16,9 +16,16 @@ public class Main {
 
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
-			String nextLine;
-			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+			int caseNum = Integer.parseInt(r.readLine());
+			for(int i=0 ; i<caseNum ; i++) {
+				String[] args = r.readLine().split("\\s+");
+				int sum = Integer.parseInt(args[0]);
+				int diff = Integer.parseInt(args[1]);
+				if ( sum >= diff && sum %2 == diff % 2) {
+					w.write("" + (sum/2+diff/2 + (sum % 2 ==0 ? 0 : 1 ) ) + " " + (sum/2-diff/2) + "\n");
+				} else {
+					w.write("impossible\n");
+				}
 			}
 			r.close();
 			w.close();
