ec803b842b80121c76ecf732fb4cf437f230afe4
mTwTm <osimpleo@gmail.com>
2016-04-13 04:27:00 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,24 +1,52 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10924 Prime Words
+// Current Status: Accepted
+// Last Submitted: 2016-04-12 20:27:13
+// Run Time: 0.030
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
+	private static final int MAX_VALUE = 52 * 20;
 	private void run() {
 		try {
 
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
+			List<Integer> primes = new ArrayList<>();
+			primes.add(2);
+findprime:
+			for(int i=3 ; i<=MAX_VALUE ; i+=2) {
+				for(int nextPrime : primes) {
+					if(i%nextPrime==0) {
+						continue findprime;
+					}
+				}
+				primes.add(i);
+			}
+			primes.add(0, 1);
 			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+				int sum = 0;
+				for(int i=0 ; i<nextLine.length() ; i++) {
+					int ascii = nextLine.charAt(i);
+					if (ascii>='a'&&ascii<='z') {
+						sum += ascii - 'a' + 1;
+					} else if (ascii>='A'&&ascii<='Z') {
+						sum += ascii - 'A' + 1 + 26;
+					}
+				}
+				if( primes.contains(sum) ) {
+					w.write("It is a prime word.\n");
+				} else {
+					w.write("It is not a prime word.\n");
+				}
 			}
 			r.close();
 			w.close();
