ae14724a98affc03e11069b67582d7903250052c
mTwTm <osimpleo@gmail.com>
2016-04-13 04:06:45 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,13 +1,14 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10929 You can say 11
+// Current Status: Accepted
+// Last Submitted: 2016-04-12 20:07:27
+// Run Time: 0.190
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 
 public class Main {
 
@@ -16,9 +17,17 @@ public class Main {
 
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
-			String nextLine;
-			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+			while(true) {
+				String nextLine = r.readLine().trim();
+				BigInteger b = new BigInteger(nextLine);
+				if(b.equals(BigInteger.ZERO)) {
+					break;
+				}
+				if(b.mod(BigInteger.valueOf(11)).equals(BigInteger.ZERO)) {
+					w.write(nextLine + " is a multiple of 11.\n");
+				} else {
+					w.write(nextLine + " is not a multiple of 11.\n");
+				}
 			}
 			r.close();
 			w.close();
