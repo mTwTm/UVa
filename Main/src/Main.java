ae4fba946fe6c3c0fba90b339e1a84f03b31276d
mTwTm <osimpleo@gmail.com>
2016-04-13 05:04:09 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10783 Odd Sum
+// Current Status: Accepted
+// Last Submitted: 2016-04-12 21:05:31
+// Run Time: 0.030
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -16,9 +16,18 @@ public class Main {
 
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
-			String nextLine;
-			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+			int caseNum = Integer.parseInt(r.readLine());
+			for(int i=0 ; i<caseNum ; i++) {
+				int start = Integer.parseInt(r.readLine());
+				int end = Integer.parseInt(r.readLine());
+				if(start%2==0) {
+					start++;
+				}
+				if(end%2==0) {
+					end--;
+				}
+				int ans = start > end ? 0 : (start+end)/2 * ((end-start)/2+1);
+				w.write("Case " + (i+1) + ": " + ans + "\n");
 			}
 			r.close();
 			w.close();
