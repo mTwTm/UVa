a9ea4ebd1ad4d58955d9ac91cb0a9092794b3787
mTwTm <osimpleo@gmail.com>
2016-05-04 23:43:12 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10340 All in All
+// Current Status: Accepted
+// Last Submitted: 2016-05-04 15:44:04
+// Run Time: 0.080
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,7 +18,21 @@ public class Main {
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+				String[] args = nextLine.split("\\s+");
+				int index0 = 0;
+				for(int index1 = 0 ; index1< args[1].length() ; index1++ ) {
+					if(args[1].charAt(index1) == args[0].charAt(index0)) {
+						index0++;
+					}
+					if(index0 == args[0].length()) {
+						break;
+					}
+				}
+				if(index0 == args[0].length()) {
+					w.write("Yes\n");
+				} else {
+					w.write("No\n");
+				}
 			}
 			r.close();
 			w.close();
