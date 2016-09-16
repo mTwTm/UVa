7a2358ad2308fa9ec2ca03ab23033a6345c250a2
Henry Yu <osimpleo@gmail.com>
2016-09-16 19:48:56 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,13 +1,9 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10327 Flip Sort
+// Current Status: Accepted
+// Last Submitted: 2016-09-16 11:40:23
+// Run Time: 0.220
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
 
@@ -18,7 +14,31 @@ public class Main {
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+				int length;
+				// Seems to have some ill-formed lines
+				try {
+					length = Integer.valueOf(nextLine.trim());
+				} catch (NumberFormatException ex) {
+					continue;
+				}
+				int[] list = new int[length];
+				int index = 0;
+				while(index != length) {
+					String[] values = r.readLine().split("\\D+");
+					for (int i = 0; i < values.length; i++) {
+						list[index + i] = Integer.valueOf(values[i]);
+					}
+					index += values.length;
+				}
+				int answer = 0;
+				for(int i = 0 ; i < list.length ; i++) {
+					for(int j = 0 ; j < i ; j++) {
+						if(list[j] > list[i]) {
+							answer++;
+						}
+					}
+				}
+				w.write("Minimum exchange operations : " + answer + "\n");
 			}
 			r.close();
 			w.close();
