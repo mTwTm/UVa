05e91f509c2218a124c5a3f43b1bb8d06e8fb2c4
mTwTm <osimpleo@gmail.com>
2016-09-16 23:52:43 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10300 Ecological Premium
+// Current Status: Accepted
+// Last Submitted: 2016-09-16 15:52:52
+// Run Time: 0.030
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -17,8 +17,16 @@ public class Main {
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
+			r.readLine();
 			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+				int count = Integer.valueOf(nextLine.trim());
+				long answer = 0;
+				for(int i=0 ; i<count ; i++) {
+					nextLine = r.readLine();
+					String[] farm = nextLine.split("\\D+");
+					answer += Long.valueOf(farm[0]) * Long.valueOf(farm[2]);
+				}
+				w.write(answer+"\n");
 			}
 			r.close();
 			w.close();
