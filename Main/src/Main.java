5742e158ecc4130f5fe29821841d931148fc0228
mTwTm <osimpleo@gmail.com>
2016-09-17 23:19:26 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10195 The Knights Of The Round Table
+// Current Status: Accepted
+// Last Submitted: 2016-09-17 15:19:12
+// Run Time: 0.180
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,7 +18,18 @@ public class Main {
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+				String[] lengthStrings = nextLine.trim().split("\\s+");
+				double a = Double.valueOf(lengthStrings[0]);
+				double b = Double.valueOf(lengthStrings[1]);
+				double c = Double.valueOf(lengthStrings[2]);
+				double s = (a+b+c)/2;
+				double radius;
+				if(s!=0) {
+					radius = Math.sqrt(s * (s - a) * (s - b) * (s - c)) / s;
+				} else {
+					radius = 0;
+				}
+				w.write(String.format("The radius of the round table is: %.3f\n", radius));
 			}
 			r.close();
 			w.close();
