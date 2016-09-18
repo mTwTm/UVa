d14da09e2515df289018c96483a6bee846a2e632
mTwTm <osimpleo@gmail.com>
2016-09-18 15:59:50 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,7 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10161 Ant on a Chessboard
+// Current Status: Accepted
+// Last Submitted: 2016-09-18 08:00:00
+// Run Time: 0.040
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,7 +18,22 @@ public class Main {
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+				int num = Integer.parseInt(nextLine.trim());
+				if(num==0) {
+					break;
+				}
+				int base = (int) Math.sqrt(num);
+				int remainder = num - base * base;
+				int x = 0;
+				int y = 0;
+				if(base%2==0) {
+					x = base + (remainder == 0 ? 0 : 1) + (remainder > base + 1 ? (-1 * (remainder - base - 1)) : 0);
+					y = 1 + (remainder > 1 ? (remainder > base+1 ? base : remainder - 1) : 0);
+				} else {
+					y = base + (remainder == 0 ? 0 : 1) + (remainder > base + 1 ? (-1 * (remainder - base - 1)) : 0);
+					x = 1 + (remainder > 1 ? (remainder > base+1 ? base : remainder - 1) : 0);
+				}
+				w.write(String.format("%d %d\n", x, y));
 			}
 			r.close();
 			w.close();
