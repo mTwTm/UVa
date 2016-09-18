29d20b664a9bb0cf33a7a92f19067538ca87d63d
mTwTm <osimpleo@gmail.com>
2016-09-18 21:13:11 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,13 +1,15 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10107 What is the Median?
+// Current Status: Accepted
+// Last Submitted: 2016-09-18 13:00:26
+// Run Time: 0.100
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
 
@@ -17,8 +19,37 @@ public class Main {
 			BufferedReader r = new BufferedReader(new InputStreamReader(System.in, "ISO-8859-1"));
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
+			PriorityQueue<Integer> head = new PriorityQueue<>(1, Collections.reverseOrder());
+			PriorityQueue<Integer> tail = new PriorityQueue<>();
+			int lastMedian = Integer.valueOf(r.readLine().trim());
+			boolean sizeIsOdd = true;
+			w.write(lastMedian+"\n");
 			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+				int next = Integer.valueOf(nextLine.trim());
+				if(sizeIsOdd) {
+					if(next>lastMedian) {
+						head.add(lastMedian);
+						tail.add(next);
+					} else {
+						head.add(next);
+						tail.add(lastMedian);
+					}
+					lastMedian = (head.peek() + tail.peek())/2;
+				} else {
+					int leftMedian = head.peek();
+					int rightMedian = tail.peek();
+					if(next >= leftMedian && next <= rightMedian) {
+						lastMedian = next;
+					} else if (next < leftMedian){
+						lastMedian = head.poll();
+						head.add(next);
+					} else if (next > rightMedian){
+						lastMedian = tail.poll();
+						tail.add(next);
+					}
+				}
+				sizeIsOdd = !sizeIsOdd;
+				w.write(lastMedian+"\n");
 			}
 			r.close();
 			w.close();
