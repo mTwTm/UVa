3445335142ab89b5c2c552b46471f4a1b49ed2f6
mTwTm <osimpleo@gmail.com>
2016-09-18 21:41:17 +0800

    AC

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,13 +1,14 @@
-// UVa <ID> <Name> 
-// Current Status: Accepted|Pending|TooSlow
-// Last Submitted: 
-// Run Time: 
+// UVa 10106 Product
+// Current Status: Accepted
+// Last Submitted: 2016-09-18 13:41:35
+// Run Time: 0.040
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 
 public class Main {
 
@@ -18,7 +19,9 @@ public class Main {
 			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
 			String nextLine;
 			while( ( nextLine = r.readLine() ) != null ) {
-				w.write(nextLine);
+				BigInteger b = new BigInteger(nextLine.trim());
+				b = b.multiply(new BigInteger(r.readLine().trim()));
+				w.write(b.toString()+"\n");
 			}
 			r.close();
 			w.close();
